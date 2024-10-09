3ee730ef15977a4da08f6f2f2fa8e5c82f85fa77
Bharath <bharathmuthyala0902@gmail.com>
2024-10-09 12:34:38 +0530

    jh vs
    askdj

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// Simple signup logic 
+
 function signup() {
     const username = document.getElementById("signup-username").value;
     const email = document.getElementById("signup-email").value;
     const password = document.getElementById("signup-password").value;
 
     if (username && email && password) {
-        // Storing user data in localStorage
+       
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
         alert("Signup successful! You can now login.");
-        window.location.href = 'login.html';  // Redirect to login page
+        window.location.href = 'login.html'; 
     } else {
         alert("Please fill in all fields.");
     }
 }
 
-// Simple login logic
+
 function login() {
     const username = document.getElementById("login-username").value;
     const password = document.getElementById("login-password").value;
 
-    // Retrieving stored data from localStorage
+   
     const storedUsername = localStorage.getItem('username');
     const storedPassword = localStorage.getItem('password');
 
     if (username === storedUsername && password === storedPassword) {
-        window.location.href = 'Main.html';  // Redirect to main page
+        window.location.href = 'Main.html';  
     } else {
         alert("Invalid username or password.");
     }
